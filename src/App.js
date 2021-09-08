757a3c289ae454bf2ad9c888ed352b168c5c589e
churst73 <chelsea.ann.hurst@gmail.com>
2021-09-08 17:17:22 -0400

    Built search engine and added font

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import logo from "./logo.png";
+import Dictionary from "./Dictionary.js";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello there</header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">coded by chelsea hurst</footer>
+      </div>
     </div>
   );
 }
