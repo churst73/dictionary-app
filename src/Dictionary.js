8db175f9bd735cda16f491506e5a1ab6a87ba331
churst73 <chelsea.ann.hurst@gmail.com>
2021-09-11 15:45:28 -0400

    Added Pexels Api for Photo section

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,35 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results.js";
 import "./Dictionary.css";
+import Photos from "./Photos.js";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000017716362fc1304bec9a03fa13b6645483";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios
+      .get(pexelsApiUrl, {
+        headers: headers,
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -54,6 +70,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />{" "}
       </div>
     );
   } else {
