59f83a9007a6ba71484339251ca52ef79bbbad5d
churst73 <chelsea.ann.hurst@gmail.com>
2021-09-11 16:19:11 -0400

    Added leaf image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Dictionary from "./Dictionary.js";
 import "./App.css";
+import camping from "./camping.png";
 
 export default function App() {
   return (
@@ -7,7 +8,13 @@ export default function App() {
       <div className="container">
         <header className="App-header">
           <h2>a dictionary app by chelsea hurst</h2>
-          <h1 className="mb-5">WHAT DOES THAT MEAN?</h1>
+          <h1 className="mb-4">WHAT DOES THAT MEAN?</h1>
+          <img
+            src={camping}
+            width="830px"
+            alt="camping"
+            className="img-fluid mb-5"
+          ></img>
         </header>
         <main>
           <Dictionary defaultKeyword="royal" />
